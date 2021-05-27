dd3950a0e992f19ce6d9177b126468d91fca50ee
Sergey Kharchuk <sticaman160493@gmail.com>
2021-05-27 12:55:43 +0300

    add mailer php, add animations

--- src/js/script.js
+++ src/js/script.js
@@ -83,4 +83,35 @@ $(document).ready(function(){
   // Masked
 
   $('input[name=phone]').mask("+7(999) 999-99-99");
+
+  $('form').submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+      type: "POST",
+      url: "mailer/smart.php",
+      data: $(this).srialize()
+    }).done(function() {
+      $(this).find("input").val("");
+
+
+      $('form').trigger('reset');
+    });
+    return false;
+  });
+
+  // Smooth scroll and pageup
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 1000) {
+      $('.pageup').fadeIn();
+    } else {
+      $('.pageup').fadeOut();
+    }
+  });
+
+  $("a[href^='#']").click(function(){
+    const _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;
+  });
 });
